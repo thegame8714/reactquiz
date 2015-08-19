8f4be17ce7e63ebe3aca6d854f67d7cebc36a19e
Fabio Salimbeni <Fabio@adminisatorsair.home>
2015-08-19 23:17:59 +0100

    Quiz #2

--- src/quiz.js
+++ src/quiz.js
@@ -3,24 +3,80 @@
  */
 'use strict';
 
+var MulipleAnswer = React.createClass({
+  render: function() {
+    return (<span>This needs to be implemented</span>);
+  }
+});
+
+var FillTheGap = React.createClass({
+  render: function() {
+    return (<span>This needs to be implemented</span>);
+  }
+});
+
 var Question_Type = React.createClass({
-   render: function() {
-       return (
-           <select className="qurestiontype">
-                <option name="fillthegap" value="fillthegap">Fill the gap</option>
-                <option name="yesno" value="yesno">Yes/No</option>
-                <option name="multipleanswer" value="multipleanswer">Multiple Answer</option>
-           </select>
-       );
-   }
+    getInitialState:function(){
+      return {selectValue:''};
+  },
+    handleChange:function(e){
+    this.setState({selectValue:e.target.value});
+  },
+  render: function() {
+    var answertype=this.state.selectValue;
+    var answers,button;
+
+    switch(answertype) {
+      case 'yesno':
+        answers = (<div><label>
+                    <input type="radio" name="yesno_radio" value="Yes"/>
+                    <span>Yes</span>
+                </label>
+                <label>
+                    <input type="radio" name="yesno_radio" value="No"/>
+                    <span>No</span>
+                </label></div>); 
+        break;
+      case 'multipleanswer':
+        answers = (<div><label>
+                    <input type="checkbox" name="yesno_check" value="0"/>
+                    <span>Yes</span>
+                </label>
+                <label>
+                    <input type="checkbox" name="yesno_check" value="0"/>
+                    <span>No</span>
+                </label></div>);
+        break;
+      case "fillthegap":
+        answers = (<input type="text" value="this is the answer" />);
+        break;
+      default:
+        answers = (<label>No type selected yet</label>);
+    }
+
+    return (
+      <div>
+      <select value={this.state.selectValue} onChange={this.handleChange} >
+        <option value="">Select an answer type</option>
+        <option value="yesno">Yes/No</option>
+        <option value="multipleanswer">Multiple Answer</option>
+        <option value="fillthegap">Fill the gap</option>
+      </select>
+      <div>{answers}</div>
+      </div>        
+    );
+  }
 });
 
 var Question = React.createClass({
    render: function() {
        return (
            <div>
-           <h1>This is question number {this.props.count + 1} </h1>
-            <Question_Type onChange={this.props.selectType} />
+           <h3>Question {this.props.numberOfQuestion}:</h3>
+           <input type="text" name="question{this.props.numberOfQuestion}" />
+            <Question_Type />
+            <button onClick={this.props.addQuestion}>Add Question</button>
+            <button onClick={this.props.removeQuestion}>Remove Question</button>
        </div>
        );
    }
@@ -28,14 +84,55 @@ var Question = React.createClass({
 
 var Quiz = React.createClass({
     getInitialState: function() {
-      answers = []
+        return {
+          numberOfQuestion: 1,
+          rows: [],
+          addNew: true
+        };
     },
-
+    addQuestion: function () {
+      this.setState({
+        numberOfQuestion: this.state.numberOfQuestion + 1,
+        addNew: true
+      });
+    },
+    removeQuestion: function () {
+      console.log(this.state.numberOfQuestion);
+      if(this.state.numberOfQuestion > 1) {
+        this.setState({
+        numberOfQuestion: this.state.numberOfQuestion - 1,
+        addNew: false
+      });
+      }
+      // if(this.state.numberOfQuestion == 1) {
+      //  this.setState({
+      //   // numberOfQuestion: this.state.numberOfQuestion - 1,
+      //   addNew: false
+      // });
+      // }
+    },
     render: function() {
-        var rows = [];
-        for (var i=0; i<this.props.numberOfQuestion; i++) {
-            rows.push(<Question count={i} />);
-        }
+        var rows = this.state.rows;
+        var noq = this.state.numberOfQuestion;
+        var addNew = this.state.addNew;
+        console.log(noq);
+        if(addNew) {
+           rows.push(<Question numberOfQuestion={noq} 
+                            addQuestion={this.addQuestion}
+                            removeQuestion={this.removeQuestion} />);  
+         }else {
+          if(noq > 1) {
+            rows.pop();  
+          }
+          else {
+            if(noq == 1) {
+              alert("You can't have 0 questions");
+            }
+          }
+            
+         }
+        
+       
         return (
             <div className="well">
             {rows}
@@ -44,6 +141,6 @@ var Quiz = React.createClass({
     }
 });
 React.render(
-<Quiz numberOfQuestion="10" />,
+<Quiz />,
     document.getElementById('content')
 );
